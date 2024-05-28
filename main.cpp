b69e38d7393ae2c1f106167151a9c44e7fe9cbd5
Alexia Niculae <94938165+AlexiaNi@users.noreply.github.com>
2024-05-28 11:02:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -5,255 +6,10 @@
 
 using namespace std;
 
-ifstream fin ("grafurineorientate.in");
+ifstream fin("grafurineorientate.in");
 
-int n, x[101][101], a, b;
-
-
-void grafuri_partiale(){
-    
-}
-
-void arbore (int k)
-{
-
-  int viz[30], u, v, p, c[30], m = 0;
-
-  for (int i = 1; i <= n - 1; i++)
-	for (int j = i + 1; j <= n; j++)
-	  {
-		x[j][i] = x[i][j];
-		m += x[i][j];
-	  }
-
-  for (int j = 1; j <= n; j++)
-	viz[j] = 0;
-
-
-  c[1] = k;
-  p = 1;
-  u = 1;
-  viz[k] = 1;
-  while (p <= u)
-	{
-	  v = c[p];
-	  for (int i = 1; i <= n; i++)
-		{
-
-		  if ((x[v][i] == 1) && (viz[i] == 0))
-
-			{
-
-			  u++;
-			  c[u] = i;
-			  viz[i] = 1;
-
-			}
-
-		}
-	  p++;
-	}
-  int arbore = 1;
-  for (int i = 1; i <= n; i++)
-	if (!viz[1])
-	  arbore = 0;
-  if (m == n - 1 && arbore)
-	cout << "Graful este arbore";
-  else
-	cout << "Graful nu este arbore";
-}
-
-void conex ()
-{
-  int a[105], v[105], st, dr, cont=0;
-
-  st = dr = 1;
-  v[1] = 1;
-  a[1] = 1;
-  while (st <= dr)
-	{
-	  int t = a[st];
-	  for (int i = 1; i <= n; ++i)
-		if (v[i] == 0 && x[t][i] == 1)
-		  {
-			dr++;
-			v[i] = 1;
-			a[dr] = i;
-		  }
-	  st++;
-	}
-  for (int i = 1; i <= n; ++i){
-	if (v[i] == 0)
-	  cont++;
-}
-  if(cont==0)
-    cout << "Graful este conex";
-  else
-    cout << "Graful nu este conex";
-	    
-}
-
-
-void noduriincidente (int k)
-{
-  int count = 0;
-  for (int i = 1; i <= n; i++)
-	{
-	  for (int j = i + 1; j <= n; j++)
-		{
-		  if (x[i][j] == 1)
-			{
-
-			  count++;
-			  if (count == k)
-				{
-				  cout << "Nodurile incidente cu cea de a " << k <<
-					"-a muchie a grafului sunt: " << i << " si " << j;
-				  return;
-				}
-			}
-		}
-	}
-  cout << "Nu exista a " << k << "-a muchie in graf." << endl;
-}
-
-
-void noduriadiacente (int k)
-{
-  for (int i = 1; i <= n; i++)
-	{
-	  if (x[k][i] == 1)
-		cout << i << " ";
-	}
-}
-
-void gradmaxsimin ()
-{
-  int grad_minim = n;
-  int grad_maxim = 0;
-  int noduri_grad_minim = 0;
-  int noduri_grad_maxim = 0;
-
-  for (int i = 1; i <= n; i++)
-	{
-	  int grad_nod = 0;
-	  for (int j = 1; j <= n; j++)
-		{
-		  if (x[i][j] == 1)
-			{
-			  grad_nod++;
-			}
-		}
-
-	  if (grad_nod < grad_minim)
-		{
-		  grad_minim = grad_nod;
-		  noduri_grad_minim = 1;
-		}
-	  else if (grad_nod == grad_minim)
-		{
-		  noduri_grad_minim++;
-		}
-
-	  if (grad_nod > grad_maxim)
-		{
-		  grad_maxim = grad_nod;
-		  noduri_grad_maxim = 1;
-		}
-	  else if (grad_nod == grad_maxim)
-		{
-		  noduri_grad_maxim++;
-		}
-	}
-
-  cout << "Nodurile de grad minim sunt: ";
-  for (int i = 1; i <= n; i++)
-	{
-	  int grad_nod = 0;
-	  for (int j = 1; j <= n; j++)
-		{
-		  if (x[i][j] == 1)
-			{
-			  grad_nod++;
-			}
-		}
-	  if (grad_nod == grad_minim)
-		{
-		  cout << i << " ";
-		}
-	}
-  cout << endl;
-
-  cout << "Nodurile de grad maxim sunt: ";
-  for (int i = 1; i <= n; i++)
-	{
-	  int grad_nod = 0;
-	  for (int j = 1; j <= n; j++)
-		{
-		  if (x[i][j] == 1)
-			{
-			  grad_nod++;
-			}
-		}
-	  if (grad_nod == grad_maxim)
-		{
-		  cout << i << " ";
-		}
-	}
-  cout << endl;
-}
-
-void noduriterminale ()
-{
-  int noduri_terminale = 0;
-  cout << "Nodurile terminale sunt: ";
-  for (int i = 1; i <= n; i++)
-	{
-	  int grad_nod = 0;
-	  for (int j = 1; j <= n; j++)
-		{
-		  if (x[i][j] == 1)
-			{
-			  grad_nod++;
-			}
-		}
-	  if (grad_nod == 1)
-		{
-		  cout << i << " ";
-		  noduri_terminale++;
-		}
-	}
-  if (noduri_terminale == 0)
-	{
-	  cout << "nu exista noduri terminale.";
-	}
-  cout << endl;
-}
-
-
-void afiseazaCicluriLungime4() {
-    bool cicluriGasite = false;
-    for (int i = 1; i <= n; ++i) {
-        for (int j = i + 1; j <= n; ++j) {
-            if (x[i][j]) {
-                for (int k = j + 1; k <= n; ++k) {
-                    if (x[j][k]) {
-                        for (int l = k + 1; l <= n; ++l) {
-                            if (x[k][l] && x[l][i]) {
-                                cout << "Ciclu de lungime 4: " << i << " " << j << " " << k << " " << l << " " << i << endl;
-                                cicluriGasite = true;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    if (!cicluriGasite) {
-        cout << "Nu exista cicluri de lungime 4 in graf." << endl;
-    }
-}
-
+int n, x[101][101], a, b,m,c1,c2;
+int viz[101];
 
 bool verificaLant(const vector<int>& lant) {
     for (size_t i = 0; i < lant.size() - 1; ++i) {
@@ -338,108 +94,512 @@ void citesteSiVerificaCiclu() {
 }
 
 
+void afiseazaCicluriLungime4() {
+    bool cicluriGasite = false;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = i + 1; j <= n; ++j) {
+            if (x[i][j]) {
+                for (int k = j + 1; k <= n; ++k) {
+                    if (x[j][k]) {
+                        for (int l = k + 1; l <= n; ++l) {
+                            if (x[k][l] && x[l][i]) {
+                                cout << "Ciclu de lungime 4: " << i << " " << j << " " << k << " " << l << " " << i << endl;
+                                cicluriGasite = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (!cicluriGasite) {
+        cout << "Nu exista cicluri de lungime 4 in graf." << endl;
+    }
+}
 
 
 
-int main ()
+
+void noduriincidente(int k) {
+    int count = 0;
+    for(int i = 1; i <= n; i++) {
+        for(int j = i + 1; j <= n; j++) {
+            if(x[i][j] == 1)
+            {
+
+                count++;
+                if(count == k) {
+                    cout << "Nodurile incidente cu cea de a " << k << "-a muchie a grafului sunt: " << i << " si " << j;
+                    return;
+                }
+            }
+        }
+    }
+    cout << "Nu exista a " << k << "-a muchie in graf." << endl;
+}
+
+
+void verif_ciclu(int a[101][101], int n) {
+    int k[101], p;
+
+    cout <<"Introduceti numarul de noduri: ";
+    cin >> p;
+    cout <<"Introduceti nodurile care trebuie vizitate: ";
+    for (int i = 1; i <= p; ++i) {
+        cin >> k[i];
+    }
+
+
+    if (p < 2 || k[1] != k[p]) {
+        cout << "Nu este un ciclu";
+        return;
+    }
+
+
+    bool visited[101] = {false};
+    visited[k[1]] = true;
+    for (int i = 2; i < p; ++i) {
+        if (k[i] < 1 || k[i] > n || visited[k[i]]) {
+            cout << "Nu este un ciclu";
+            return;
+        }
+        visited[k[i]] = true;
+    }
+
+    cout << "Este un ciclu";
+}
+
+
+void dfs(int k, int *v)
 {
-  fin >> n;
-  while (fin >> a >> b)
-	{
-	  x[a][b] = 1;
-	  x[b][a] = 1;
-	}
-  cout << "\nMeniu:" << endl;
-  cout << "1. Afiseaza matricea de adiacenta" << endl;
-  cout << "2. Afiseaza, pentru un nod citit, gradul nodului" << endl;
-  cout << "3. Afiseaza nodurile incidente cu a k-a muchie" << endl;
-  cout << "4. Afiseaza nodurile adiacente pentru un nod dat" << endl;
-  cout << "5. Afiseaza nodurile de grad maxim, respectiv minim" << endl;
-  cout << "7. Sa se afiseze nodurile terminale" << endl;
-  cout << "14. Citeste si verifica un lant" << endl; 
-  cout << "15. Citeste si verifica un lant elementar" << endl; 
-  cout << "16. Citeste si verifica un ciclu" << endl; 
-  cout << "11. Sa se verifice daca exista un lant de lungime 4" << endl;  
-  cout << "19. Sa se verifice daca graful este conex" << endl;
-  cout << "20. Sa se verifice daca graful este un arbore" << endl;  
-  cout << "0. Iesire" << endl;
+    v[k] = 1;
+    v[1001]++;
+    for (int i = 1; i <= n; i++)
+        if (x[k][i] == 1 && v[i] == 0)
+        {
+            dfs(i, v);
+        }
+}
 
-  int option;
-  cout << "Alege optiunea: ";
-  cin >> option;
-  switch (option)
-	{
+int cerinta19()
+{
+    int v[1002] = {0};
+    dfs(1, v);
+    if (v[1001] == n)
+        return 1;
+    else
+        return 0;
+}
 
-	case 1:
-	  {
-		matricea_de_adiacenta ();
-		break;
-	  }
 
-	case 3:
-	  {
-		int k;
-		cout << "Introduceti indexul muchiei (k): ";
-		cin >> k;
-		noduriincidente (k);
-		break;
-	  }
-	case 4:
-	  {
-		int k;
-		cout << "Nodurile adiacente sunt: ";
-		noduriadiacente (k);
-		break;
-	  }
-	case 5:
-	  {
-		gradmaxsimin ();
-		break;
-	  }
-	case 7:
-	  {
-		noduriterminale ();
-		break;
-	  }
-	case 14:
-	  {
-		itesteSiVerificaLant();
-		break;
-	  }
-	case 15:
-	  {
-		citesteSiVerificaLantElementar()
-		break;
-	  }
-	case 16:
-	  {
-		citesteSiVerificaCiclu()
-		break;
-	  }
-	case 11:
+void cerinta20()
+{
+     if (cerinta19() == 0)
+         cout << "Nu e arbore!" << '\n';
+     else
+          cout << "E arbore!" << '\n';
+}
+
+
+void noduriadiacente(int k)
+{
+   for(int i = 1; i <= n; i++) {
+        if(x[k][i]==1)
+        cout<<i<<" ";
+    }
+}
+
+void gradmaxsimin() {
+    int grad_minim = n;
+    int grad_maxim = 0;
+    int noduri_grad_minim = 0;
+    int noduri_grad_maxim = 0;
+
+    for (int i = 1; i <= n; i++) {
+        int grad_nod = 0;
+        for (int j = 1; j <= n; j++) {
+            if (x[i][j] == 1) {
+                grad_nod++;
+            }
+        }
+
+        if (grad_nod < grad_minim) {
+            grad_minim = grad_nod;
+            noduri_grad_minim = 1;
+        } else if (grad_nod == grad_minim) {
+            noduri_grad_minim++;
+        }
+
+        if (grad_nod > grad_maxim) {
+            grad_maxim = grad_nod;
+            noduri_grad_maxim = 1;
+        } else if (grad_nod == grad_maxim) {
+            noduri_grad_maxim++;
+        }
+    }
+
+    cout << "Nodurile de grad minim sunt: ";
+    for (int i = 1; i <= n; i++) {
+        int grad_nod = 0;
+        for (int j = 1; j <= n; j++) {
+            if (x[i][j] == 1) {
+                grad_nod++;
+            }
+        }
+        if (grad_nod == grad_minim) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    cout << "Nodurile de grad maxim sunt: ";
+    for (int i = 1; i <= n; i++) {
+        int grad_nod = 0;
+        for (int j = 1; j <= n; j++) {
+            if (x[i][j] == 1) {
+                grad_nod++;
+            }
+        }
+        if (grad_nod == grad_maxim) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+void noduriterminale() {
+    int noduri_terminale = 0;
+    cout<<"Nodurile terminale sunt: ";
+    for (int i = 1; i <= n; i++) {
+        int grad_nod = 0;
+        for (int j = 1; j <= n; j++) {
+            if (x[i][j] == 1) {
+                grad_nod++;
+            }
+        }
+        if(grad_nod == 1) {
+            cout << i << " ";
+            noduri_terminale++;
+        }
+    }
+    if (noduri_terminale == 0) {
+        cout << "nu exista noduri terminale.";
+    }
+    cout << endl;
+}
+
+
+
+void matricea_de_adiacenta ()
+{
+
+cout<<"Matricea de adiacenta a grafului este:"<<endl;
+    for(int i = 1 ; i <= n ; i++)
+        {
+            for(int j = 1 ; j <= n ; j++)
+                {
+                    cout << x[i][j] << " ";
+            }cout << endl;
+        }
+}
+
+void grad_nod(int k)
+{
+    int g[101];
+      for(int i = 1 ;  i <= n ; i++){
+        g[i] = 0;
+        for(int j = 1; j <= n ; j++)
+            if(x[i][j] == 1)
+                g[i] ++;
+    }
+    cout<<g[k];
+}
+
+
+void noduri_izolate()
+{
+    int g[101];
+     for(int i = 1 ;  i <= n ; i++){
+        g[i] = 0;
+        for(int j = 1; j <= n ; ++j)
+            if(x[i][j] == 1)
+                g[i] ++;
+    }
+
+
+    for(int i = 1 ;  i <= n ; i++)
+    {
+        if(g[i] == 0)
+            cout << i << " ";
+    }
+
+}
+
+
+void dfs(int nod)
+{
+    viz[nod]=1;
+    cout<<nod<<' ';
+    for(int i=1;i<=n;i++)
+        if(x[nod][i]==1 && viz[i]==0)
+        dfs(i);
+
+}
+
+void BFS(int nod)
+{
+    int p = 0, u = 0, q[1001], viz[1001] = {};
+    q[0] = nod;
+    viz[nod] = 1;
+    while (p <= u)
+    {
+        nod = q[p];
+        p++;
+        cout << nod << " ";
+        for (int i = 1; i <= n; i++)
+        {
+            if (x[i][nod] == 1 && viz[i] == 0)
+            {
+                viz[i] = 1;
+                u++;
+                q[u] = i;
+            }
+        }
+    }
+}
+
+void lantelementar(int c1, int c2, int *L, int *X, int k, int ok)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (!L[i])
+        {
+            L[i] = 1;
+            X[k] = i;
+            if (k == 1 || x[X[k - 1]][X[k]] == 1)
+            {
+                if (X[k] == c2)
+                {
+                    ok = true;
+                    cout << "Lantul este:";
+                    for (int j = 1; j <= k; j++)
+                        cout << X[j] << " ";
+                    cout << '\n';
+                    return;
+                }
+                else if (!ok)
+                {
+                    lantelementar(c1, c2, L, X, k + 1, ok);
+                }
+            }
+            L[i] = 0;
+        }
+    }
+}
+
+int cerinta10()
+{
+    int v[101]={0};
+    for(int i=1;i<=n;i++)
+    for(int j=1;j<=i;j++)
+    {
+    if(x[ i][j]==1)
+    if(v[i]==1 && v[j]==1)
+    return 1;
+    else
+    {
+    v[i]=1;v[j]=1;
+    }
+    }
+    return 0;
+}
+
+void op9(int x1, int* vizat)
+{
+    vizat[x1]=1;
+    cout<<x1<<" ";
+
+    for(int i = 1; i <= n; i++){
+        if(x[x1][i] == 1 && vizat[i]==0)
+        {
+            op9(i,vizat);
+        }
+    }
+}
+
+int main() {
+    fin >> n;
+    while(fin >> a >> b) {
+        x[a][b] = 1;
+        x[b][a] = 1;
+    }
+    cout << "\nMeniu:" << endl;
+    cout << "1. Afiseaza matricea de adiacenta" << endl;
+    cout << "2. Afiseaza, pentru un nod citit, gradul nodului" << endl;
+    cout << "3. Afiseaza nodurile incidente cu a k-a muchie" << endl;
+    cout << "4. Afiseaza nodurile adiacente pentru un nod dat" << endl;
+    cout << "5. Afiseaza nodurile de grad maxim, respectiv minim" << endl;
+    cout << "6. Afiseaza nodurile izolate" << endl;
+    cout << "7. Sa se afiseze nodurile terminale" << endl;
+    cout << "8. Sa se afiseze un lant elementar de la nodul x la y" << endl;
+    cout << "9. Citeste un nod x si afiseaza toate nodurile legate printr-un lant de x" << endl;
+    cout << "10. Verifica daca gradul are cicluri" << endl;
+    cout << "11. Sa se verifice daca exista un lant de lungime 4" << endl;
+    cout << "12. Parcurgere DFS" << endl;
+    cout << "13.BFS"<<endl;
+    cout << "14. lant" << endl;
+    cout << "15. lant elementar" << endl;
+    cout << "16. ciclu" << endl;
+    cout << "17. verifica ciclu" << endl;
+    cout << "19. Sa se verifice daca graful este conex" << endl;
+    cout << "20. Sa se verifice daca graful este un arbore" << endl;
+    cout << "0. Iesire" << endl;
+
+    int option;
+    cout << "Alege optiunea: ";
+    cin >> option;
+    switch(option) {
+
+       case 1: {
+            matricea_de_adiacenta();
+            break;
+        }
+      case 2: {
+          int k;
+	cout<< "Introduceti nodul dorit:";
+	cin>>k;
+	if(k>n)
+    {
+        cout<<"Nodul nu exista!";
+        break;
+    }
+    else
+           grad_nod (k);
+            break;
+        }
+
+
+        case 3: {
+            int k;
+
+            cout << "Introduceti indexul muchiei (k): ";
+            cin >> k;
+            noduriincidente(k);
+            break;
+        }
+        case 4: {
+            int k;
+            cout<<"Introduceti nodul dorit: ";
+            cin>>k;
+            cout<<"Nodurile adiacente sunt: ";
+            noduriadiacente(k);
+            break;
+        }
+        case 5:{
+            gradmaxsimin( );
+            break;
+        }
+       case 6: {
+            noduri_izolate();
+            break;
+        }
+        case 7:{
+            noduriterminale();
+            break;
+     }
+
+ case 8:
+        {
+            cout << "Introduceti cele doua noduri:";
+            cin >> c1 >> c2;
+            int L[1001] = {0}, X[1001] = {0};
+            bool ok = false;
+            L[c1] = 1;
+            X[1] = c1;
+            lantelementar(c1, c2, L, X, 2, ok);
+            break;
+        }
+
+
+        case 9:
+        {
+            int ponct,vizat[101]={0};
+            cin>>ponct;
+            op9(ponct,vizat);
+            break;
+        }
+        case 10:
+        {
+            if (cerinta10() == 1)
+                cout << "Exista cicluri" << endl;
+            else
+                cout << "Nu exista cicluri" << endl;
+            break;
+       case 11:
 	  {
 		afiseazaCicluriLungime4();
 		break;
 	  }
+      case 12:
+ {
+                int x0;
+                cout<<"Numarul de muchii al grafului este:";
+                cin>>m;
+                cout<<"Nodul din care va incepe parcurgerea grafului este:";
+                cin>>x0;
+                dfs(x0);
+                break;
+        }
+
+       case 13: // parcurgere bfs
+        {
+            int k;
+            cin >> k; // nodul din care incepe parcurgerea
+            BFS(k);
+            break;
+        }
+	case 14:
+	  {
+		citesteSiVerificaLant();
+		break;
+	  }
+	case 15:
+	  {
+		citesteSiVerificaLantElementar();
+		break;
+	  }
+	case 17:
+        {
+        	int p;
+        	verif_ciclu(x,n);
+        	break;
+        }
+	case 16:
+	  {
+		citesteSiVerificaCiclu();
+		break;
+	  }
 	case 19:
 	  {
-        	conex ();
+              if (cerinta19() == 1)
+                cout << "Graful este conex!" << endl;
+              else
+              	cout << "Graful nu este conex!" << endl;
 		break;
 	  }
 	case 20:
 	  {
-	    	int k;
-		cout << "Introduceti nodul de plecare: ";
-		cin >> k;
-		arbore (k);
+		cerinta20 ();
 		break;
 	  }
-	default:
-	  {
-		cout << "Optiune invalida! Va rugam sa alegeti din nou." << endl;
-		break;
-	  }
-	}
+        default: {
+            cout << "Optiune invalida! Va rugam sa alegeti din nou." << endl;
+            break;
+        }
+    }
 
-  return 0;
+    }
+    return 0;
+
 }
+
